748a5f6d734fe681e01380ecf95149041321013b
CHansY <maks.chibotaru@gmail.com>
2022-10-23 22:37:47 +0300

    Updates

---
+++ precache-manifest.ffc3a2d16342fa61d9acf5d941782d52.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe686c85c7a4e1effa74",
+    "url": "/CHansYtheBest/ReactSocial/static/css/main.9565c1a5.chunk.css"
+  },
+  {
+    "revision": "fe686c85c7a4e1effa74",
+    "url": "/CHansYtheBest/ReactSocial/static/js/main.fe686c85.chunk.js"
+  },
+  {
+    "revision": "9875db2efa6e6d5c0233",
+    "url": "/CHansYtheBest/ReactSocial/static/js/1.9875db2e.chunk.js"
+  },
+  {
+    "revision": "be1b12166cc0e4fd4831",
+    "url": "/CHansYtheBest/ReactSocial/static/js/2.be1b1216.chunk.js"
+  },
+  {
+    "revision": "f2675d1dd64d082a958d",
+    "url": "/CHansYtheBest/ReactSocial/static/js/runtime~main.f2675d1d.js"
+  },
+  {
+    "revision": "c16ab624c25cf69a2e5d9ea1ad0afffe",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Search.c16ab624.svg"
+  },
+  {
+    "revision": "ee7d7cae32ad782e9b7ad99616f32b84",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Dialog.ee7d7cae.svg"
+  },
+  {
+    "revision": "8cb64d96ac67ea81abcf2e0478bc54d4",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Bell.8cb64d96.svg"
+  },
+  {
+    "revision": "b739b27c761f3c58c7c5b35836f4de8f",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Logo.b739b27c.svg"
+  },
+  {
+    "revision": "1dab545b1b256aab9418987756d2a029",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Profile.1dab545b.svg"
+  },
+  {
+    "revision": "52e80b6fcfaa9764e82a2b97f9dec292",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Feed.52e80b6f.svg"
+  },
+  {
+    "revision": "27e86842a143c2f3d5a78ec6583b5c50",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Message.27e86842.svg"
+  },
+  {
+    "revision": "70e92cc5a7dd2698e38de2fbf36d3b19",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Friends.70e92cc5.svg"
+  },
+  {
+    "revision": "5689f1167331da240f340b24c6e8288f",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Settings.5689f116.svg"
+  },
+  {
+    "revision": "d173cafad1bb23f6e8d6469d8bd9e3d2",
+    "url": "/CHansYtheBest/ReactSocial/static/media/Copyright.d173cafa.svg"
+  },
+  {
+    "revision": "a426785786f3993435e4cf5ea15f6315",
+    "url": "/CHansYtheBest/ReactSocial/index.html"
+  }
+];
